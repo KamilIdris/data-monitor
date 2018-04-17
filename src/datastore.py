4d33ecb90b59ac7bc404950120a7b5a3822cba11
Kamil Idris <38122255+KamilIdris@users.noreply.github.com>
2018-04-17 16:06:14 +0800

    Split db file based on current date.
    Signed-off-by: Kamil Idris <38122255+KamilIdris@users.noreply.github.com>

--- src/datastore.py
+++ src/datastore.py
@@ -2,21 +2,34 @@
 """
 SQLite interface for data storage
 
-Updated 2nd March 2018 by Kamil
+Updated 11th April 2018 by Kamil
 """
 
 import dataset
-   
+from time import localtime, strftime
+ 
 class DataStore(object):
     """
     The DataStore is used to handle all connections with the SQLite database 
     """
-    def __init__(self, dbname='data'):
-        self.dbname = dbname
+    def __init__(self):
+        self.updatedbname()
+    
+    def updatedbname(self):
+        """
+        DB name is based on current date, this ensures file
+        """
+        identifier = strftime('%Y%m%d', localtime())
+        self.dbname = 'sqlite:///db/' + identifier + '.db'
         
     def insert(self, data):
         """
         Inserts additional data into the data store
         """
+        self.updatedbname()
+        
         with dataset.connect(self.dbname) as tx:
             tx['tbl'].insert(data)
+            
+    def read(self):
+        pass
